33997e866e00ef5fa836df9dd6e05be696e5e64a
MD NIMUR HASAN <developernaim20@gmail.com>
2024-06-20 01:43:40 +0600

    2 Screen add

--- app/src/main/java/com/nehalappstudio/nectarecommerce/StartScreen.kt
+++ app/src/main/java/com/nehalappstudio/nectarecommerce/StartScreen.kt
@@ -1,12 +1,18 @@
 package com.nehalappstudio.nectarecommerce
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import com.nehalappstudio.nectarecommerce.pages.GetStartScreen
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.util.Timer
 
-class MainActivity : AppCompatActivity() {
+class StartScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,12 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        lifecycleScope.launch {
+            delay(150)
+            startActivity(Intent(this@StartScreen, GetStartScreen::class.java))
+        }
+
+
     }
 }
